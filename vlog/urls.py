2aa092592b15733a5bfa4684b5277e374b0def01
sivaprakash007-sp-cmd <sp945610@gmail.com>
2026-02-18 13:27:08 +0530

    initial django project upload

---
+++ vlog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='vlog-index' ),
+    path("post/<int:post_id>", views.detail, name="detail" ),
+    path('new_url/', views.new_url_view, name='new_url' ),
+    path('old_url', views.old_url_redirect, name='old_url' ),
+]
---
+++ vlog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World, You are at vlog's index")
+
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing post detail page. And ID is 4")
+
+def old_url_redirect(request):
+    return redirect("new_url")
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
