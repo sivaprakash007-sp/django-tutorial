f299a824fa208fa4365230fb3e5738e5b8ebcd34
sivaprakash007-sp-cmd <sp945610@gmail.com>
2026-02-18 14:05:05 +0530

    new file created

--- vlog/views.py
+++ vlog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World, You are at vlog's index")
+    return render(request,'index.html')
 
 def detail(request, post_id):
     return HttpResponse(f"You are viewing post detail page. And ID is 4")
